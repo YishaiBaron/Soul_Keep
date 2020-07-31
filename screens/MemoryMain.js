66011948db86f7e0593f8c357cae3cf0a1ce3b25
Yishai Baron <yishai.baron@gmail.com>
2020-07-31 11:19:49 +0300

    Upload Files

--- screens/MemoryMain.js
+++ screens/MemoryMain.js
@@ -6,7 +6,7 @@ import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 const MemoryMain = (props) => {
     return (
         <View style={{alignItems: 'center', marginTop:40}}>
-        <Text>Hello kjhh {props.name}
+        <Text>Hello 
         </Text>
       </View>
     );
